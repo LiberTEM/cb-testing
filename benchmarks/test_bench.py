fd46f5b3e67430601a01b96ddca02b598a996870
Alexander Clausen <a.clausen@fz-juelich.de>
2025-05-28 16:40:58 +0200

    Add a second benchmark

--- benchmarks/test_bench.py
+++ benchmarks/test_bench.py
@@ -1,6 +1,10 @@
 import pytest
-from cb_testing import f
+from cb_testing import f, g
 
 
-def test_bench(benchmark):
+def test_bench_f(benchmark):
     benchmark(f)
+
+
+def test_bench_g(benchmark):
+    benchmark(g)
